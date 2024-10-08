b3b156037d83e3522e0fc508322a6ae10f925454
Eliot Fagnon <efagnon2002@gmail.com>
2024-10-08 11:47:08 +0100

    feat: error handling

--- lib/actions/scrap-page.ts
+++ lib/actions/scrap-page.ts
@@ -7,28 +7,31 @@ import { scrapeNovojob } from "../services/novojob-scrapper";
 import { scrapeWiijob } from "../services/wiijob-scrapper";
 
 export async function scrapPage(website: string, url: string) {
-  if (website === "cadreannonces.com") {
-    const jobs = await scrapeCadreAnnonces(url);
-    console.log(jobs);
-    return jobs;
-  } else if (website === "bjemploi.com") {
-    const jobs = await scrapeBjEmploi(url);
-    console.log(jobs);
-    return jobs;
-  } else if (website === "emploibenin.com") {
-    console.log(website);
-    const jobs = await scrapeEmploiBenin(url);
-    console.log(jobs);
-    return jobs;
-  } else if (website === "novojob.com") {
-    const jobs = await scrapeNovojob(url);
-    console.log(jobs);
-    return jobs;
-  } else if (website === "wiijob.com") {
-    const jobs = await scrapeWiijob(url);
-    console.log(jobs);
-    return jobs;
+  try {
+    if (website === "cadreannonces.com") {
+      const jobs = await scrapeCadreAnnonces(url);
+      console.log(jobs);
+      return jobs;
+    } else if (website === "bjemploi.com") {
+      const jobs = await scrapeBjEmploi(url);
+      console.log(jobs);
+      return jobs;
+    } else if (website === "emploibenin.com") {
+      console.log(website);
+      const jobs = await scrapeEmploiBenin(url);
+      console.log(jobs);
+      return jobs;
+    } else if (website === "novojob.com") {
+      const jobs = await scrapeNovojob(url);
+      console.log(jobs);
+      return jobs;
+    } else if (website === "wiijob.com") {
+      const jobs = await scrapeWiijob(url);
+      console.log(jobs);
+      return jobs;
+    }
+  } catch (error) {
+    console.log(error);
   }
-
   return [];
 }
